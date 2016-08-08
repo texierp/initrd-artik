f1d7161e62fc4bb1a3a127186cf00a23d0ab9ca8
Chanho Park <chanho61.park@samsung.com>
2016-08-08 19:53:48 +0900

    sd_recovery: set performance mode during fusing
    This patch sets system performance mode during emmc
    recovery to shorten the fusing time. It will unset
    cpu hotplug and set cpu governor to performance.
    This will also set the devfreq to prevent decreasing
    memory bandwidth.
    
    Change-Id: Ifc3a40621dc0c8504c7bacc30af1844544568c9c
    Signed-off-by: Chanho Park <chanho61.park@samsung.com>

--- scripts/sbin/sd_recovery.sh
+++ scripts/sbin/sd_recovery.sh
@@ -10,6 +10,29 @@ SD_MNT=/root/sd_root
 mkdir -p $SD_MNT
 mkdir /mnt
 
+set_performance_mode()
+{
+	# disable hotplug
+	if [ -e /sys/devices/system/cpu/hotplug/force_hstate ]; then
+		echo 0 > /sys/devices/system/cpu/hotplug/force_hstate
+	fi
+
+	# set performance mode
+	if [ -e /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor ]; then
+		echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+	fi
+
+	# set performance mode for HMP
+	if [ -e /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor ]; then
+		echo performance > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+	fi
+
+	# set devfreq to maximum for nexell
+	if [ -e /sys/class/devfreq/nx-devfreq/min_freq ]; then
+		echo 400000 > /sys/class/devfreq/nx-devfreq/min_freq
+	fi
+}
+
 fuse_rootfs_partition()
 {
 	mkfs.ext4 -F $EMMC_ROOT_DEV -L rootfs > /dev/null 2>&1
@@ -19,6 +42,8 @@ fuse_rootfs_partition()
 	cp $SD_MNT/artik_release /mnt/etc > /dev/null 2>&1
 	sync; sync; sync
 }
+
+set_performance_mode
 
 sync
 mdev -s
