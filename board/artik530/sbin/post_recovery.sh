f2b97c5323aae148ca242dc6555497ee86f15cdb
Chanho Park <chanho61.park@samsung.com>
2016-10-19 04:54:16 +0900

    artik530: split pre/post script from artik710
    This patch splits pre/post script of artik530 from artik710.
    
    Change-Id: Ia1ecbb9003bdb93217715cac72a136d4172b54b0
    Signed-off-by: Chanho Park <chanho61.park@samsung.com>

--- board/artik530/sbin/post_recovery.sh
+++ board/artik530/sbin/post_recovery.sh
@@ -1,1 +1,21 @@
-../../artik710/sbin/post_recovery.sh
+#!/bin/sh
+
+LED_RED=28
+LED_BLUE=38
+GPIO_PATH=/sys/class/gpio
+
+led_ctl()
+{
+	local led=$1
+	local ctl=$2
+
+	if ! [ -d $GPIO_PATH/gpio$led ]; then
+		echo $led > $GPIO_PATH/export
+	fi
+
+	echo "out" > $GPIO_PATH/gpio$led/direction
+	echo $ctl > $GPIO_PATH/gpio$led/value
+}
+
+led_ctl $LED_RED 0
+led_ctl $LED_BLUE 1
