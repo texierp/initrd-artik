cd6dad2ee648cf69b0def9c99ecee116f4c0c46b
Chanho Park <chanho61.park@samsung.com>
2017-03-02 19:07:44 +0900

    artik530: support zigbee firmware upgrade
    This patch supports zigbee firmware upgrade for artik530.
    The firmware file is located /sbin directory and check the
    firmware version before starting the sd recovery stage.
    After firmware fusing is done, the zigbee f/w version will
    be also checked to judge whether the fw upgrade is correct.
    If there is a thread firmware, the thread firmware will be
    not flashed and version check will be skipped.
    
    Change-Id: If62af21f9e5a57293658125cdacdca1fe5238d33
    Signed-off-by: Chanho Park <chanho61.park@samsung.com>
    Signed-off-by: Joonghong Park <joonghong.park@samsung.com>

--- board/artik530/sbin/post_recovery.sh
+++ board/artik530/sbin/post_recovery.sh
@@ -3,6 +3,7 @@
 LED_RED=28
 LED_BLUE=38
 GPIO_PATH=/sys/class/gpio
+SCRIPT_DIR=`dirname "$(readlink -f "$0")"`
 
 led_ctl()
 {
@@ -19,3 +20,19 @@ led_ctl()
 
 led_ctl $LED_RED 0
 led_ctl $LED_BLUE 1
+
+# Zigbee firmware check
+
+CUR_DIR=$(pwd)
+cd $SCRIPT_DIR
+./802.15.4_setup.sh Artik530_EFR32MG1B232F256GM32_xncp-uart-rts-cts-use-with-serial-btl-5740-0003-0007.ebl "5.7.4 GA build 99 xNCP 0x7" 1 > /dev/null 2>&1
+RET=$?
+cd $CUR_DIR
+
+if [ $RET == 0 ]; then
+	echo "The zigbee fw version is the latest"
+	exit 0
+else
+	echo "Invalid zigbee fw version"
+	exit 1
+fi
